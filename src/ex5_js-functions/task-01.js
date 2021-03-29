d7e4054e01470195d83681ae5f6cca93ff1a0d90
KhanMu1998 <deadcrafter1998@gmail.com>
2021-03-29 19:02:22 +0300

    task fix 2

--- src/ex5_js-functions/task-01.js
+++ src/ex5_js-functions/task-01.js
@@ -3,41 +3,42 @@ let Calculator = {
 
   reset() {
     Calculator.result = 0;
-    return Calculator.result;
+    return Calculator.getResult;
   },
 
   getResult() {
-    return Calculator.result;
+    console.log(Calculator.result);
+    return Calculator.getResult;
   },
 
   add(value) {
-    if (value === undefined) return Calculator.result;
+    if (value === undefined) return Calculator.add;
     Calculator.result += value;
-    return Calculator.result;
+    return Calculator.add;
   },
 
   subtract(value) {
-    if (value === undefined) return Calculator.result;
+    if (value === undefined) return Calculator.subtract;
     Calculator.result -= value;
-    return Calculator.result;
+    return Calculator.subtract;
   },
 
   divide(value) {
-    if (value === undefined) return Calculator.result;
+    if (value === undefined) return Calculator.divide;
 
     if ((value === 0) | (value === Infinity)) {
       console.log("Недопустимая операция");
-      return Calculator.result;
+      return Calculator.divide;
     }
 
     Calculator.result /= value;
-    return Calculator.result;
+    return Calculator.divide;
   },
 
   multiply(value) {
-    if (value === undefined) return Calculator.result;
+    if (value === undefined) return Calculator.multiply;
     Calculator.result *= value;
-    return Calculator.result;
+    return Calculator.multiply;
   },
 };
 
